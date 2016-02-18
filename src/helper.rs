8fc575a530e73b143a63850affe90a7cec9ff5c4
Lewin Bormann <lbo@spheniscida.de>
2016-02-18 14:57:16 +0100

    Implement generic regex extractor

--- src/helper.rs
+++ src/helper.rs
@@ -2,6 +2,12 @@
 
 use std::fs;
 use std::io::Read;
+use std::iter::FromIterator;
+use std::iter::empty;
+use std::str::FromStr;
+
+extern crate regex;
+use self::regex::Regex;
 
 /// Reads and returns a full procfs file.
 /// Example:
@@ -40,4 +46,41 @@ pub fn get_procfs_file_lines(path: String) -> Option<Vec<String>> {
 
 pub fn commaseparated_to_vec(s: String) -> Vec<String> {
     s.split(",").map(String::from).collect()
+}
+
+pub fn extract_from_str<T: FromStr + Clone, C: FromIterator<T>>(s: &String,
+                                                                re: &Regex,
+                                                                default: T)
+                                                                -> C {
+    match re.captures(&*s) {
+        None => empty().collect(),
+        Some(caps) => {
+            caps.iter()
+                .skip(1)
+                .map(|cap| {
+                    cap.map_or(default.clone(),
+                               |s| T::from_str(s).unwrap_or(default.clone()))
+                })
+                .collect()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate regex;
+    use self::regex::Regex;
+    use super::*;
+
+    #[test]
+    fn test_extract_1() {
+        let re = Regex::new(r"(\d+) (\d+)").unwrap();
+        let s = String::from("123 456");
+
+        let result: Vec<i32> = extract_from_str(&s, &re, 0);
+
+        assert_eq!(result[0], 123);
+        assert_eq!(result[1], 456);
+        assert_eq!(result.len(), 2);
+    }
 }
